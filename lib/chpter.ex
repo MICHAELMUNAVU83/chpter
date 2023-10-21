b8a883bb66a6a7e352977e635b8843c98a67ece5
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2023-10-21 16:49:09 +0300

    Setup Repository

---
+++ lib/chpter.ex
@@ -0,0 +1,91 @@
+defmodule Chpter do
+  def initiate_payment(
+        api_key,
+        phone_number,
+        name,
+        email,
+        amount,
+        location,
+        callback_url,
+        transaction_reference
+      ) do
+    header = header(api_key)
+
+    body =
+      body(
+        phone_number,
+        email,
+        name,
+        location,
+        amount,
+        callback_url,
+        transaction_reference
+      )
+
+    url = "https://api.chpter.co/v1/initiate/mpesa-payment"
+
+    request_body = Poison.encode!(body)
+
+    HTTPoison.post(url, request_body, header)
+  end
+
+  defp header(api_key) do
+    [
+      {
+        "Content-Type",
+        "application/json"
+      },
+      {
+        "Api-Key",
+        api_key
+      }
+    ]
+  end
+
+  defp body(
+         phone_number,
+         email,
+         name,
+         location,
+         amount,
+         callback_url,
+         transaction_reference
+       ) do
+    %{
+      customer_details: %{
+        "full_name" => name,
+        "location" => location,
+        "phone_number" => phone_number,
+        "email" => email
+      },
+      products: [],
+      amount: %{
+        "currency" => "KES",
+        "delivery_fee" => 0.0,
+        "discount_fee" => 0.0,
+        "total" => amount
+      },
+      callback_details: %{
+        "transaction_reference" => transaction_reference,
+        "callback_url" => callback_url
+      }
+    }
+  end
+
+  def check_for_payment(transaction_reference, api_endpoint) do
+    body = HTTPoison.get!(api_endpoint)
+
+    customer_record =
+      Poison.decode!(body.body)["data"]
+      |> Enum.find(fn record -> record["transaction_reference"] == transaction_reference end)
+
+    customer_record
+
+    if customer_record != nil do
+      customer_record
+    else
+      Process.sleep(1000)
+      check_for_payment(transaction_reference, api_endpoint)
+    end
+  end
+end
---
+++ mix.exs
@@ -0,0 +1,54 @@
+defmodule Chpter.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :chpter,
+      version: "0.1.0",
+      elixir: "~> 1.12",
+      start_permanent: Mix.env() == :prod,
+      description:
+        "This is wrapper for Chpter API that you can use to integrate Mpesa payments in your Elixir application.",
+      deps: deps(),
+      package: package(),
+      name: "Chpter",
+      source_url: "https://github.com/manuelgeek/mpesa_elixir.git",
+      homepage_url: "https://hexdocs.pm/mpesa/Mpesa.html",
+      docs: [
+        # The main page in the docs
+        main: "Mpesa",
+        # logo:
+        #   "https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/M-PESA_LOGO-01.svg/1200px-M-PESA_LOGO-01.svg.png",
+        extras: ["README.md"]
+      ]
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Michael Munavu"],
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/manuelgeek/mpesa_elixir.git",
+        "Documentation" => "https://hexdocs.pm/mpesa/Mpesa.html",
+        "README" => "https://hexdocs.pm/mpesa/readme.html"
+      }
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:poison, "~> 5.0"},
+      {:timex, "~> 3.0"},
+      {:httpoison, "~> 2.1"}
+    ]
+  end
+end
